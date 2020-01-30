57a14a7f20f69c688d97f6b009da82786c3cb509
Meshal Al-Sultan <51976236+meshalalsultan@users.noreply.github.com>
2020-01-30 20:08:01 +0300

    Add files via upload

---
+++ tweets-collector-master/api_keys.py
@@ -0,0 +1,4 @@
+consumer_key = 'xgkA30ZnNtxpx5rZ4M2ZeADyt'
+consumer_secret = 'xUiCmURo5NVXYVeNrL2pvO8br4B355zGBWrAwOCfa1L1yweZtE'
+access_token = '1184086168608088064-jPANfIKR0OsxoubRlO6t8RhdyJ7LCf'
+access_secret = 'PhgH1FVI2Gkg2JFYchlcuS2PHz0Y6XGpjD1ifIbUpP932'
